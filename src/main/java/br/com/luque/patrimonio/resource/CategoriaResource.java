b07ddf171baba6628efd7d500eebf05e8285d0c4
Luciano Queiroz <luciano.queiroz@gmail.com>
2021-10-30 13:52:03 -0300

    Criando  pacote resource

---
+++ src/main/java/br/com/luque/patrimonio/resource/CategoriaResource.java
@@ -0,0 +1,29 @@
+package br.com.luque.patrimonio.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.luque.patrimonio.config.ApplicationConstants;
+import br.com.luque.patrimonio.domain.dto.CategoriaDTO;
+import br.com.luque.patrimonio.domain.entities.Categoria;
+import br.com.luque.patrimonio.services.CategoriaService;
+
+@RestController
+@RequestMapping(ApplicationConstants.API_VERSION)
+public class CategoriaResource {
+
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @PostMapping(path = "/categoria")
+    public ResponseEntity<Categoria> postCategoria(@RequestBody CategoriaDTO body){
+        Categoria response = categoriaService.adicionar(body);
+        return new ResponseEntity<Categoria>(response, HttpStatus.CREATED);
+    }
+    
+}
